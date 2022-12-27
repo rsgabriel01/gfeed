7d0beb989b0a6cf792c9611644d500670bbb0e17
Gabriel Rodrigues <rsgabriel01@gmail.com>
2022-12-26 23:39:04 -0300

    css modules

--- Aulas/projeto-01-fundamentos-reactjs/src/App.jsx
+++ Aulas/projeto-01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from './components/header'
 import { Post } from './Post'
+
+import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Gabriel Rodrigues"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quis veniam provident exercitationem dolorum molestias fuga? Id consectetur culpa, doloremque minima sed voluptatem totam. Deserunt dicta accusamus quam repellendus eligendi?"
