707040aaa43e45c4f229aa68907da803e8f4193c
Gabriel Rodrigues <rsgabriel01@gmail.com>
2023-01-10 00:00:23 -0300

    Finalizado até aula de sidebar

--- Aulas/projeto-01-fundamentos-reactjs/src/App.jsx
+++ Aulas/projeto-01-fundamentos-reactjs/src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from './components/header'
 import { Post } from './Post'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
 
 import './global.css'
 
@@ -7,11 +10,17 @@ export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Gabriel Rodrigues"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quis veniam provident exercitationem dolorum molestias fuga? Id consectetur culpa, doloremque minima sed voluptatem totam. Deserunt dicta accusamus quam repellendus eligendi?"
-      />
-      <Post author="José da Silva" content="Um novo post?" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Gabriel Rodrigues"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quis veniam provident exercitationem dolorum molestias fuga? Id consectetur culpa, doloremque minima sed voluptatem totam. Deserunt dicta accusamus quam repellendus eligendi?"
+          />
+          <Post author="José da Silva" content="Um novo post?" />
+        </main>
+      </div>
     </div>
   )
 }
