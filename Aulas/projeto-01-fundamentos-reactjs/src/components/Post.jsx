a171a0b0c1df50ba955f5b4a24c93d52cf5323cf
Gabriel Rodrigues <rsgabriel01@gmail.com>
2023-02-27 22:26:40 -0300

    finalizado aula "Componente: Post"

---
+++ Aulas/projeto-01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,41 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            classname={styles.avatar}
+            src="https://github.com/rsgabriel01.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Gabriel Rodrigues</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time
+          title="27 de fevereiro de 2023 às 22:20h"
+          dateTime="2023-02-27 22:20:18"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉<a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
