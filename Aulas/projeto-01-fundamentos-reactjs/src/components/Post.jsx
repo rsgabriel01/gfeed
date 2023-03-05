968a437fba202d70baff5460dfad0ac6b56686a0
Gabriel Rodrigues <rsgabriel01@gmail.com>
2023-03-04 21:15:39 -0300

    Criado componente Comment

--- Aulas/projeto-01-fundamentos-reactjs/src/components/Post.jsx
+++ Aulas/projeto-01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -48,6 +49,12 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
