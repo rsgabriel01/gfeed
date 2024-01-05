5acc2fdb3242513059056208285f94f216a1a4f0
Gabriel Rodrigues <rsgabriel01@gmail.com>
2024-01-04 22:17:23 -0300

    Alter file folders

---
+++ src/components/Comment.jsx
@@ -0,0 +1,43 @@
+import { Avatar } from './Avatar'
+
+import styles from './Comment.module.css'
+
+import { ThumbsUp, Trash } from 'phosphor-react'
+
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Diego Fernandes</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
