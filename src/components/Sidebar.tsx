d7c5da863bcf38a56ec60ebaa5ab92b274edac19
gabriel.souza <gabriel.souza@aliare.co>
2024-01-25 13:39:57 -0300

    alter color scheme

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,7 +9,7 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img
         className={styles.cover}
-        src="https://images.unsplash.com/photo-1704975986930-0c09f513c985?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        src="https://images.unsplash.com/photo-1532292060982-8bfb986808e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
 
       <div className={styles.profile}>
