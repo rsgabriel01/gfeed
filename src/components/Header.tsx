69e7212a1a480041c0d9f3ed0f09fbb5e6848b9a
Gabriel Rodrigues <rsgabriel01@gmail.com>
2024-01-26 14:55:07 -0300

    add complements of symbol

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 import styles from './Header.module.css'
 
-import igniteLogo from '../assets/g-feed.svg'
+import igniteLogo from '../../public/g-feed_logo.svg'
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={igniteLogo} alt="Logotipo do Ignite" />
+      <img src={igniteLogo} alt="Logotipo do Ignite" /> <span>G-Feed</span>
     </header>
   )
 }
